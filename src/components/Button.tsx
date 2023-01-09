849fb768d62a23c41f8596ac9a908077160cfd38
Brenner Spear <blspear@gmail.com>
2023-01-09 16:03:01 -0600

    global styling stuff

---
+++ src/components/Button.tsx
@@ -0,0 +1,50 @@
+// const Button = ({ text, onClick }: { text: string; onClick: () => any }) => {
+//     return (
+//         <button
+//             type="button"
+//             className="inline-flex items-center rounded-md border border-transparent bg-teal-100 px-4 py-2 text-base font-medium text-black hover:bg-teal-200 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
+//             onClick={onClick}
+//         >
+//             {text}
+//         </button>
+//     )
+// }
+
+export const enum ButtonType {
+    Primary = 'primary',
+    Secondary = 'secondary',
+    // Tertiary = 'tertiary',
+}
+
+const ButtonTypeClass = {
+    primary:
+        'inline-flex items-center rounded-md border border-transparent bg-teal-300 px-4 py-2 text-base font-medium text-black  shadow-sm hover:bg-teal-400 focus:outline-none focus:ring-1 focus:ring-black focus:ring-offset-1',
+    secondary:
+        'inline-flex items-center rounded-md border border-transparent bg-[#2D3836] px-4 py-2 text-base font-medium text-teal-50 hover:bg-[#1B2221] focus:outline-none focus:ring-1 focus:ring-teal-50 focus:ring-offset-1',
+}
+
+const Button = ({
+    text,
+    onClick,
+    type,
+}: {
+    text: string
+    onClick: () => any
+    type: ButtonType
+}) => {
+    return (
+        <button
+            type="button"
+            className={ButtonTypeClass[type]}
+            onClick={onClick}
+        >
+            {text}
+        </button>
+    )
+}
+
+Button.defaultProps = {
+    type: ButtonType.Primary,
+}
+
+export default Button
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,4 @@
+import Button, { ButtonType } from 'components/Button'
 import Navbar from 'components/Navbar'
 import { type NextPage } from 'next'
 
@@ -18,21 +19,22 @@ const Home: NextPage = () => {
                 {invitations.map(
                     ({ role, organization, organizationId, status }) => {
                         return status === 'PENDING' ? (
-                            <div key={`${organizationId}_${role}`}>
+                            <div
+                                key={`${organizationId}_${role}`}
+                                className="flex items-center gap-2"
+                            >
                                 <div className="text-lg">
                                     {organization.name}
                                 </div>
-                                <button
-                                    className="bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+                                <Button
+                                    text="Accept Invitation"
                                     onClick={() =>
                                         mutation.mutate({
                                             organizationId,
                                             role,
                                         })
                                     }
-                                >
-                                    Accept Invitation
-                                </button>
+                                />
                             </div>
                         ) : null
                     },
@@ -66,7 +68,7 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Navbar />
-            <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#85C9C1] to-[#15162c]">
+            <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-black to-black">
                 <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
                     <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
                         Avat
@@ -76,23 +78,14 @@ const Home: NextPage = () => {
                     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
                         <h3 className="text-2xl font-bold">Organizations</h3>
                         <Organizations />
-                        <h3 className="text-2xl font-bold">Projects</h3>
-                        <div className="text-lg">{user?.projects[0]?.name}</div>
+                        <h3 className="text-2xl font-bold"></h3>
+                        <Invitations />
                     </div>
-                    <div className="flex flex-col items-center gap-2">
-                        <p className="text-2xl text-white">
-                            {/* {hello.data
-                                ? hello.data.greeting
-                                : 'Loading tRPC query...'} */}
-                        </p>
-                    </div>
-                    <Invitations />
-                    <button
-                        className="bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+                    <Button
+                        text="Sign Out"
                         onClick={() => signOut()}
-                    >
-                        Sign Out
-                    </button>
+                        type={ButtonType.Secondary}
+                    />
                 </div>
             </main>
         </>
