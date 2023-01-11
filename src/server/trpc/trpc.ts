d50e0cbfe33273bd0c0a1cf422dedd4a5628d563
Brenner Spear <blspear@gmail.com>
2023-01-11 12:00:35 -0600

    clean up trpc.ts

--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -25,15 +25,10 @@ const isAuthed = t.middleware(({ ctx, next }) => {
     if (!ctx.session || !ctx.session.userId) {
         throw new TRPCError({ code: 'UNAUTHORIZED' })
     }
-    return next({
-        // ctx,
-        ctx: {
-            // address: ctx.session.address,
-            // infers the `session` as non-nullable
-            // session: { ...ctx.session, user: ctx.session.user },
-            session: { ...ctx.session },
-        },
-    })
+    // for some reason, prevents the session type from being null
+    // and for some reason, we don't need to pass the prisma client? but I am anyways?
+    // return next({ ctx })
+    return next({ ctx: { session: { ...ctx.session }, prisma: ctx.prisma } })
 })
 
 /**
