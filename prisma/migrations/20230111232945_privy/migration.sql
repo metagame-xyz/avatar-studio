01199a41c15fe79b5deff21b0fc3210b8c422442
Brenner Spear <blspear@gmail.com>
2023-01-11 17:30:28 -0600

    fix acceptOrgInvitation and squash migrations

--- prisma/migrations/20230111232945_privy/migration.sql
+++ prisma/migrations/20230111232945_privy/migration.sql
@@ -2,9 +2,7 @@
   Warnings:
 
   - You are about to drop the `Example` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `PrivyAccount` table. If the table is not empty, all the data it contains will be lost.
   - You are about to drop the `Session` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the column `emailVerified` on the `User` table. All the data in the column will be lost.
   - You are about to drop the column `access_token` on the `Account` table. All the data in the column will be lost.
   - You are about to drop the column `expires_at` on the `Account` table. All the data in the column will be lost.
   - You are about to drop the column `id_token` on the `Account` table. All the data in the column will be lost.
@@ -14,6 +12,8 @@
   - You are about to drop the column `scope` on the `Account` table. All the data in the column will be lost.
   - You are about to drop the column `session_state` on the `Account` table. All the data in the column will be lost.
   - You are about to drop the column `token_type` on the `Account` table. All the data in the column will be lost.
+  - You are about to drop the column `emailVerified` on the `User` table. All the data in the column will be lost.
+  - Added the required column `privyDID` to the `User` table without a default value. This is not possible if the table is not empty.
 
 */
 -- DropIndex
@@ -26,31 +26,11 @@ PRAGMA foreign_keys=on;
 
 -- DropTable
 PRAGMA foreign_keys=off;
-DROP TABLE "PrivyAccount";
-PRAGMA foreign_keys=on;
-
--- DropTable
-PRAGMA foreign_keys=off;
 DROP TABLE "Session";
 PRAGMA foreign_keys=on;
 
 -- RedefineTables
 PRAGMA foreign_keys=OFF;
-CREATE TABLE "new_User" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "firstName" TEXT,
-    "lastName" TEXT,
-    "email" TEXT,
-    "image" TEXT,
-    "address" TEXT,
-    "role" TEXT
-);
-INSERT INTO "new_User" ("address", "email", "firstName", "id", "image", "lastName", "role") SELECT "address", "email", "firstName", "id", "image", "lastName", "role" FROM "User";
-DROP TABLE "User";
-ALTER TABLE "new_User" RENAME TO "User";
-CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-CREATE UNIQUE INDEX "User_address_key" ON "User"("address");
 CREATE TABLE "new_Account" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "userId" TEXT NOT NULL,
@@ -63,10 +43,26 @@ CREATE TABLE "new_Account" (
     "number" TEXT,
     "chainType" TEXT,
     "verifiedAt" DATETIME,
-    CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("privyDID") ON DELETE CASCADE ON UPDATE CASCADE
 );
 INSERT INTO "new_Account" ("id", "type", "userId") SELECT "id", "type", "userId" FROM "Account";
 DROP TABLE "Account";
 ALTER TABLE "new_Account" RENAME TO "Account";
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "privyDID" TEXT NOT NULL,
+    "firstName" TEXT,
+    "lastName" TEXT,
+    "email" TEXT,
+    "image" TEXT,
+    "address" TEXT,
+    "role" TEXT
+);
+INSERT INTO "new_User" ("address", "email", "firstName", "id", "image", "lastName", "role") SELECT "address", "email", "firstName", "id", "image", "lastName", "role" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_privyDID_key" ON "User"("privyDID");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "User_address_key" ON "User"("address");
 PRAGMA foreign_key_check;
 PRAGMA foreign_keys=ON;
