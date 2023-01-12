364388f6bf71ea1de6fd21a1a30a7a4bb9400225
Brenner Spear <blspear@gmail.com>
2023-01-12 17:05:08 -0600

    clean prettier file

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -4,22 +4,6 @@ module.exports = {
     semi: false,
     tabWidth: 4,
     trailingComma: 'all',
-
-    importOrder: [
-        '^react$|^next$',
-        '<THIRD_PARTY_MODULES>',
-        '^env/(.*)$',
-        '^server/(.*)$',
-        '^utils(.*)$',
-        '^pages/(.*)$',
-        '^api/(.*)$',
-        '^components/(.*)$',
-        '^styles/(.*)$',
-        '^[./]',
-    ],
-    importOrderSeparation: true,
-    importOrderSortSpecifiers: true,
-    importOrderCaseInsensitive: true,
     plugins: [
         'prettier-plugin-organize-imports',
         'prettier-plugin-tailwindcss',
