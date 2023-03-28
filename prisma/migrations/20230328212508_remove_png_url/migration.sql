248828aedde61da381cf225defd337b3eef91754
Brenner Spear <blspear@gmail.com>
2023-03-28 15:25:31 -0600

    remove pngUrl

---
+++ prisma/migrations/20230328212508_remove_png_url/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `pngUrl` on the `Trait` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Trait" DROP COLUMN "pngUrl";
